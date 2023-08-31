e405dade9ac463613846c21b21dfd961d2d0222d
kirill <lekseich8@gmail.com>
2023-09-01 00:09:54 +0300

    Add connect between Category and Product

--- main/models.py
+++ main/models.py
@@ -1,25 +1,10 @@
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
-class Product(models.Model):
+
+class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.CharField(max_length=100, verbose_name='Описание')
-    image = models.ImageField(upload_to='main/',verbose_name='изображение (превью)', **NULLABLE)
-    category = models.CharField(max_length=100, verbose_name='Категория')
-    cost = models.IntegerField(verbose_name='Цена за покупку')
-    create_data = models.DateTimeField(verbose_name='Дата создания')
-    change_data = models.DateTimeField(verbose_name='Дата последнего изменения')
-
-    def __str__(self):
-        return f'{self.name}({self.category})'
-
-    class Meta:
-        verbose_name = 'продукт'
-        verbose_name_plural = 'продукты'
-
-class Сategory(models.Model):
-    name = models.CharField(max_length=100, verbose_name='Наименование')
-    description = models.CharField(max_length=100, verbose_name='Описание')
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
         return f'{self.name}({self.description})'
@@ -27,3 +12,20 @@ class Сategory(models.Model):
     class Meta:
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(verbose_name='Описание')
+    image = models.ImageField(upload_to='main/',verbose_name='изображение (превью)', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    cost = models.IntegerField(verbose_name='Цена за покупку')
+    create_data = models.DateTimeField(verbose_name='Дата создания', **NULLABLE)
+    change_data = models.DateTimeField(verbose_name='Дата последнего изменения', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}({self.description}, {self.category}, {self.cost})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
