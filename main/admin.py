fafe5d1c61b8efeb4737e4b35a09d439e85d50d9
kirill <lekseich8@gmail.com>
2023-08-31 22:20:01 +0300

    Add register for superuser

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Сategory, Product
+
+
+@admin.register(Сategory)
+class СategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
+    list_filter = ('name',)
+    search_fields = ('name', 'description',)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'cost', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
