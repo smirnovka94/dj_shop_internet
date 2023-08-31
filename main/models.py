bd827411bc138ce3b25d989c7da541109528cc82
kirill <lekseich8@gmail.com>
2023-08-31 22:10:54 +0300

    Create classes Product and Category

--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+    image = models.ImageField(upload_to='main/',verbose_name='изображение (превью)', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='Категория')
+    cost = models.IntegerField(verbose_name='Цена за покупку')
+    create_data = models.DateTimeField(verbose_name='Дата создания')
+    change_data = models.DateTimeField(verbose_name='Дата последнего изменения')
+
+    def __str__(self):
+        return f'{self.name}({self.category})'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+
+class Сategory(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.CharField(max_length=100, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}({self.description})'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
